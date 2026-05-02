e4ed1efa9ac40bed7bd94df5ff335e104e6151d2
Sonasha99 <ins81380@gmail.com>
2026-05-02 23:38:52 +0530

    Added 3Sum code in c++

--- DSA/3Sum.cpp
+++ DSA/3Sum.cpp
@@ -1,46 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
-        int n = nums.size();
+// Function definition 
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> result;
+    int n = nums.size();
 
-        // Step 1: Sort the array
-        sort(nums.begin(), nums.end());
+    sort(nums.begin(), nums.end());
 
-        // Step 2: Fix one element
-        for (int i = 0; i < n - 2; i++) {
+    for (int i = 0; i < n - 2; i++) {
 
-            // Skip duplicates for i
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        // skip duplicates
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int left = i + 1;
-            int right = n - 1;
+        int left = i + 1;
+        int right = n - 1;
 
-            // Step 3: Two-pointer approach
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
 
-                if (sum == 0) {
-                    result.push_back({nums[i], nums[left], nums[right]});
+            if (sum == 0) {
+                result.push_back({nums[i], nums[left], nums[right]});
 
-                    
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+                // skip duplicates
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
 
-                    left++;
-                    right--;
-                }
-                else if (sum < 0) {
-                    left++;  
-                }
-                else {
-                    right--; 
-                }
+                left++;
+                right--;
+            }
+            else if (sum < 0) {
+                left++;
+            }
+            else {
+                right--;
             }
         }
-        return result;
     }
-};
+    return result;
+}
+
+int main() {
+    int n; cin>>n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        int x; cin>>x;
+        nums[i] = x;
+    }
+
+    vector<vector<int>> ans = threeSum(nums);
+
+    // output print
+    for (auto &triplet : ans) {
+        for (int x : triplet) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
